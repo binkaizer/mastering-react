ae9c14fc11c43b23e2081419023b56e064d58453
Tam Pham <binemperor@gmail.com>
2019-01-16 01:37:20 +0700

    Composing components 8-update the state

--- counter-app/src/Components/counter.jsx
+++ counter-app/src/Components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.counter.value,
     //imageUrl: "https://picsum.photos/200"
     tags: ["tag1", "tag2", "tag3"]
   };
@@ -19,7 +19,7 @@ class Counter extends Component {
 
   handleIncrement = product => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   renderTags() {
@@ -34,9 +34,11 @@ class Counter extends Component {
   }
 
   render() {
+    console.log("props", this.props);
     return (
       <div>
         {/* <img src={this.state.imageUrl} alt="" /> */}
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
@@ -44,20 +46,26 @@ class Counter extends Component {
         >
           Increment
         </button>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+        {/* {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()} */}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
